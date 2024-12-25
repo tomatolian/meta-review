aa5cb062e020ad91d53f1e6d775872683843fbac
tomatolian <vinegtom@gmail.com>
2024-12-26 00:30:16 +0900

    change user_id

--- pages/chatpage.py
+++ pages/chatpage.py
@@ -27,7 +27,6 @@ def main():
     if not st.session_state.get("session_info"):
         st.switch_page("pages/question_input.py")
     
-    
     user_id="debug_user"
     chat_sessions = list(collection.find({"user_id": user_id}))
     session_num=len(chat_sessions)
